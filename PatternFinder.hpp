2092b61d2c05c640b59827dd9f6249fb941a4df3
doug157 <73317004+doug157@users.noreply.github.com>
2021-01-04 17:09:43 -0500

    Create PatternFinder.hpp

---
+++ PatternFinder.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+FORCEINLINE uintptr_t FindPattern(uintptr_t start, size_t length, const unsigned char* pattern, const char* mask)
+{
+	size_t pos = 0;
+	auto maskLength = std::strlen(mask) - 1;
+	auto startAdress = start;
+	for (auto it = startAdress; it < startAdress + length; ++it)
+	{
+		if (*reinterpret_cast<unsigned char*>(it) == pattern[pos] || mask[pos] == '?')
+		{
+			if (mask[pos + 1] == '\0')
+			{
+				return it - maskLength;
+			}
+			pos++;
+		}
+		else
+		{
+			pos = 0;
+		}
+	}
+	return -1;
+}
+
+FORCEINLINE uintptr_t FindPattern(HMODULE module, const unsigned char* pattern, const char* mask)
+{
+	MODULEINFO info = { };
+	GetModuleInformation(GetCurrentProcess(), module, &info, sizeof(MODULEINFO));
+	return FindPattern(reinterpret_cast<uintptr_t>(module), info.SizeOfImage, pattern, mask);
+}
